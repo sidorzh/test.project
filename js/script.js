93d4c3087cdd948cfc6255358c414ac101013129
Max <sidorzh@gmail.com>
2021-11-11 21:36:20 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,105 @@
+'use strict';
+
+
+const isChecked = true,
+    isClosed = false;
+
+console.log(isChecked && isClosed);
+
+
+// let incr = 10,
+//     decr = 10;
+
+// // ++incr; 
+// // --decr; 
+
+// console.log(++incr, --decr);
+
+// console.log(50 % 8);
+
+// &&
+
+
+
+
+
+// // console.log('apple ' + 'is fruit');
+
+// // console.log(1 + +'9');
+
+
+
+// const category = 'toys';
+
+// console.log('https://someurl.com/' + category + '/' + '4'); // это старый стандарт
+// console.log(`https://someurl.com/${category}/4`); // это интерполяция - 
+// //кавычки обызательно косые, а внутри - чтобы добавить переменную ставится ${переменная}
+// //и дальше без + остальной текст
+
+// const user = 'Max';
+
+// alert(`Hello, ${user}!`);
+
+// const answers = [];
+
+// answers[0] = prompt('Ваше имя?', '');
+// answers[1] = prompt('Фамилия?', '');
+// answers[2] = prompt('Сколько вам лет?', '');
+
+// console.log(typeof(null));
+
+// const result = confirm('Are you here?');
+
+// const answer = prompt('How old are you', '');
+// console.log(typeof(answer));
+
+// alert('Hello');
+
+// console.log(4/0);
+// console.log('vasya' * 100);
+
+// const person = 'Alex';
+
+// console.log(syka);
+
+// let und;
+// console.log(und);
+
+// const obj = {
+//     name: 'John',
+//     age: 25,
+//     isMarried: false
+// };
+
+// console.log(obj['name']);
+
+// // console.log(obj.name);
+
+// let arr = ['plum.png', 'orange.jpg', 'apple.bmp', 6];
+// console.log(arr[1]);
+
+
+
+
+// a = 15;
+// console.log(a);
+
+// let number = 5;
+// const leftBorderWidth = 1;
+
+// number = 10;
+// console.log(number);
+
+// const obj = {
+//     a: 50
+// }
+
+// obj.a = 10;
+
+// console.log(obj);
+
+// {
+//     let result = 50;
+// }
+
+// console.log(result);
